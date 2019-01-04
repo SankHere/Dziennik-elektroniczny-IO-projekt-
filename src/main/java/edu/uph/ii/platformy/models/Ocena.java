be1fb8261eac126be747ac12183139ecb83b83f6
SankHere <arturcio1@o2.pl>
2019-01-04 18:51:29 +0100

    Dodanie modeli, baza danych gotowa

---
+++ src/main/java/edu/uph/ii/platformy/models/Ocena.java
@@ -0,0 +1,44 @@
+package edu.uph.ii.platformy.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+
+
+@Entity
+@Table(name = "ocena")
+@Getter @Setter
+public class Ocena {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Positive
+    private double ocena;
+
+    @Valid
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_egzaminu", nullable = false)
+    private Egzamin egzamin;
+
+    @Valid
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_usera", nullable = false)
+    private User user;
+
+    public Ocena(long id, Double ocena, Egzamin egzamin, User user) {
+        this(ocena, egzamin, user);
+        this.id = id;
+    }
+
+    public Ocena(Double ocena, Egzamin egzamin, User user) {
+        this.ocena = ocena;
+        this.egzamin = egzamin;
+        this.user = user;
+    }
+}
