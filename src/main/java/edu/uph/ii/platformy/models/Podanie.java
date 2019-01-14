90f53570018ef1db4b4c5ce6f7c73ed28c5fa4d1
denied323 <demrumrus1@tlen.pl>
2019-01-14 14:31:26 +0100

    models.podanie

---
+++ src/main/java/edu/uph/ii/platformy/models/Podanie.java
@@ -0,0 +1,49 @@
+package edu.uph.ii.platformy.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "podanie")
+@NoArgsConstructor
+public class Podanie{
+
+    @Min(0)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    //zrób name w select na stronie np. rekrutacja na kierunki, utworzenie nowego przedmiotu.
+    private String name;
+
+
+    private String opis;
+
+
+    // null oczekujacy, pokazuje pracownikowi na liście z przyciskiem akceptuj zmieniajacy stan na 1
+    // 1 - zaakcceptowany przez pracownika, zmienia stan na 2
+    // 2 - zaakceptowany przez admina, pokazuje się lista adminowi i on zmienia stan na 3
+    //3 - pokazuje na liście pracownika wraz z przyciskiem do zrobienia który przekierowuje na formularz, później zmiana na 4
+    //4 zaakceptowany
+    private int stan;
+
+    public Podanie(String name, String opis, int stan){
+        this.name = name;
+        this.opis=opis;
+        this.stan=stan;
+    }
+
+    public Podanie(int id, String name,String opis, int stan){
+        this.id = id;
+        this.name = name;
+        this.opis=opis;
+        this.stan=stan;
+    }
+
+}
